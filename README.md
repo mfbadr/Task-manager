f9d4350a6088ff7f2014bc4f755b45f8d63e4a5c
mfbadr <mfbadr@gmail.com>
2014-07-10 14:24:54 -0500

    Created the task manager program

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Task-manager
 ============
+A simple program that allows the user to input tasks into one of three lists: play, school, or home.
+
+The program accepts the individual tasks, and outputs them as a list, grouped by catagory.
+
---
+++ app.js
@@ -0,0 +1,27 @@
+var chalk = require('chalk');
+var prompt = require('sync-prompt').prompt;
+
+var play = [], home = [], school = []
+var input = prompt('Which list would you like to add a task to? (p)lay, (h)ome, (s)chool, or (q)uit? ');
+var task
+
+while(input != 'q'){
+  if(input === 'p'){
+  task = prompt('What is the task you would like to add to the play list? ')
+  play.push(task)
+  }else if(input === 'h'){
+  task = prompt('What is the task you would like to add to the home list? ')
+  home.push(task)
+
+
+  }else if(input === 's'){
+  task = prompt('What is the task you would like to add to the school list? ')
+  school.push(task)
+  }
+input = prompt('Which list would you like to add a task to? (p)lay, (h)ome, (s)chool, or (q)uit? ');
+}
+
+console.log('Your play tasks are: ' + play)
+console.log('Your home tasks are: ' + home)
+console.log('Your school tasks are: ' + school)
+
